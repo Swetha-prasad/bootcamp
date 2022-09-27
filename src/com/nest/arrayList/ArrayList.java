0a9ac862e73b88cfe42a8cd013554e0f75aef442
Swetha-prasad <swethaprasad6235@gmail.com>
2022-09-27 08:21:58 +0530

    hotelbill

---
+++ src/com/nest/arrayList/ArrayList.java
@@ -0,0 +1,29 @@
+package com.nest.arrayList;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class ArrayList {
+    public static void main(String[] args) {
+        String name;
+        ArrayList<String> s=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        int a;
+        while(true)
+        {
+            System.out.println("1.employee detail");
+            System.out.println("2.view all employee");
+            System.out.println("3.exit");
+            a=sc.nextInt();
+            switch(c){
+                case 1:
+                    System.out.println("employee detail");
+                    name=sc.nextInt();
+                    s.add(name);
+                    break;
+                case 2:
+                    if (empList.size() == 0) {
+
+                    }
+            }
+        }
+    }
+}
